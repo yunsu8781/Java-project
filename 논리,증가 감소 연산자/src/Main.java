d44f47ad5294f83d1e717d75d92e03f25baad430
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-07-28 17:50:22 +0900

    first commit

---
+++ 논리,증가 감소 연산자/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String gender = "남성";
+		int height = 184;
+		
+		System.out.println(gender == "여성");
+		System.out.println(height == 180);
+		System.out.println(gender == "남성" && height ==184);
+	}
+
+}
