a0988887af3194aeb290d50cee77a47ede3bf89f
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-07-29 22:36:43 +0900

    조건문 ifelse

---
+++ 제어문(조건문 - if문, if-else문)/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		double test =5.13;
+		
+		if (test == 5.23) {
+	 		System.out.println("Áý°¡¼­ °øºÎÇÏ¼À");
+		} else {
+           System.out.println("³ë¼À¤¾¤¾");
+		}
+}
+}
